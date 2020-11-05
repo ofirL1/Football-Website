133ac779c9b962930b07a4a7974b74c80708fbcd
Ofir <ofirluzon68@gmail.com>
2020-11-05 23:01:17 +0200

    first project

---
+++ app.js
@@ -0,0 +1,8 @@
+/* hamburger */
+
+const hamburger = document.querySelector("#hamburger");
+const navUL = document.querySelector("#nav-ul");
+
+hamburger.addEventListener('click', () =>{
+    navUL.classList.toggle('show');
+});

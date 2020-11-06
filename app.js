952a05d210722811f7bd4b553ebf3f5c883b8d12
Ofir <ofirluzon68@gmail.com>
2020-11-06 16:56:09 +0200

    name classes change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* hamburger */
 
 const hamburger = document.querySelector("#hamburger");
-const navUL = document.querySelector("#nav-ul");
+const navUL = document.querySelector(".nav-ul");
 
 hamburger.addEventListener('click', () =>{
     navUL.classList.toggle('show');
